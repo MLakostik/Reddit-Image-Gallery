cff5e73a5b0d4630f625ddbdb3005f314cde3f7a
Michael <michael.lakostik@gmail.com>
2019-10-23 16:11:33 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var subReddit = "";
+setSubReddit();
+
+function setSubReddit() {
+  subReddit = document.getElementById('subreddit').value;
+  console.log(subReddit);
+
+  if (subReddit == "") {
+    subReddit = "earthporn";
+  }
+  addImages();
+}
+
+function addImages() {
+  document.getElementById('app').innerHTML = `<div class="preloader-wrapper big active">
+  <div class="spinner-layer spinner-blue-only">
+    <div class="circle-clipper left">
+      <div class="circle"></div>
+    </div><div class="gap-patch">
+      <div class="circle"></div>
+    </div><div class="circle-clipper right">
+      <div class="circle"></div>
+    </div>
+  </div>
+</div>`;
+  $.getJSON(`http://www.reddit.com/r/${subReddit}/.json?limit=100`, function(data) {
+    var content = "";
+
+    $.each(data.data.children, function(i, item) {
+      content += `<img src="${item.data.url}" class="memeImg" onerror='this.style.display = "none"' >`;
+    });
+
+    document.getElementById('app').innerHTML = "";
+    $('#app').append(content);
+  });
+}
